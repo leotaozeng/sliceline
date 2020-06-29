3ab9021bca6182ec9e2b379b527fa7dcf94708c1
Zeng Tao <qingchun1995@gmail.com>
2020-06-29 14:57:21 +0800

    Add Navbar, colors and title styles

--- src/App.js
+++ src/App.js
@@ -1,8 +1,10 @@
 import React from 'react'
 import { createGlobalStyle } from 'styled-components'
+import { Navbar } from './Navbar/Navbar'
 
 const GlobalStyle = createGlobalStyle`
   body {
+    margin: 0;
     font-family: 'Open Sans', sans-serif;
   }
 
@@ -14,8 +16,8 @@ const GlobalStyle = createGlobalStyle`
 function App() {
   return (
     <>
-      <h1>Sliceline</h1>
       <GlobalStyle />
+      <Navbar />
       <div>Hello Sliceline</div>
     </>
   )
---
+++ src/Navbar/Navbar.js
@@ -0,0 +1,28 @@
+import React from 'react'
+import styled from 'styled-components'
+import { pizzaRad } from '../Styles/colors'
+import { Title } from '../Styles/title'
+
+const NavbarStyled = styled.div`
+  background-color: ${pizzaRad};
+  padding: 10px;
+`
+
+const Logo = styled(Title)`
+  font-size: 20px;
+  color: white;
+  text-shadow: 1px 1px 4px #380502;
+`
+
+export function Navbar() {
+  return (
+    <NavbarStyled>
+      <Logo>
+        Sliceline
+        <span role="img" aria-label="logo">
+          🍕
+        </span>
+      </Logo>
+    </NavbarStyled>
+  )
+}
